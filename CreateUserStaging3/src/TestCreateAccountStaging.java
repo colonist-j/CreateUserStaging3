ee4ded67f36ae1d44ac023d734d183a06f6b4a8a
Test <illya.zhdanov@gmail.com>
2016-10-11 10:52:43 +0300

    2

--- CreateUserStaging3/src/TestCreateAccountStaging.java
+++ CreateUserStaging3/src/TestCreateAccountStaging.java
@@ -2,8 +2,7 @@ import java.io.File;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
-import org.junit.Assert;
-import org.junit.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,6 +13,8 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 
 public class TestCreateAccountStaging {
